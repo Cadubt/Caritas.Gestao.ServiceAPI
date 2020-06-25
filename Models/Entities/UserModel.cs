cf7b474490f9d3b889c95a58525e67b53f783b1c
Cadu <cadubt@gmail.com>
2020-06-24 21:04:29 -0300

    Begin the Project
    Initial Commit

---
+++ Models/Entities/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Caritas.Gestao.ServiceAPI.Models.Entities
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+
+        public String Name { get; set; }
+
+        public String Password { get; set; }
+    }
+}
