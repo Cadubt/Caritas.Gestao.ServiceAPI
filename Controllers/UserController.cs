15c27b570fb6895768451223b3c340de3e164919
Cadu <cadubt@gmail.com>
2020-06-29 14:49:34 -0300

    Including projects domain and repository

---
+++ Controllers/UserController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Caritas.Gestao.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Caritas.Gestao.ServiceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public CaritasContext _context { get; set; }
+        public UserController(CaritasContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet("filtro/{nome}")]
+        public ActionResult GetUsers(string nome)
+        {
+            
+            #region LINQ Method
+            var listUsers = _context.Users.Where(u => u.Name.Contains(nome)).ToList();
+            #endregion
+
+            #region LINQ Query
+            //var listUsers = (from user in _context.Users
+            //                 where user.Name.Contains(nome)
+            //                 select user).ToList();
+            #endregion
+
+            return Ok(listUsers);
+        }
+
+        [HttpPut("{id}")]
+        public void PutUsers(int id)
+        {
+
+        }
+
+        [HttpPost("{id}")]
+        public void PostUsers(int id)
+        {
+
+        }
+
+        [HttpDelete("{id}")]
+        public void DeleteUsers(int id)
+        {
+            var user = _context.Users.Where(u => u.Id == id).Single();
+
+            _context.Users.Remove(user);
+            _context.SaveChanges();
+
+        }
+
+
+    }
+}
---
+++ obj/Debug/netcoreapp3.1/x1ba30rv.sql
@@ -0,0 +1,25 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+CREATE TABLE [[usr]].[Users]]] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NULL,
+    [Password] nvarchar(max) NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY ([Id])
+);
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20200628135331_initial', N'3.1.5');
+
+GO
+
