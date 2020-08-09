722f402d250be9f6d04ab4abcb3ca6febca6d81b
Cadu <cadubt@gmail.com>
2020-08-09 14:13:25 -0300

    Including UserControl and Swagger into the Project

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Caritas.Gestao.Domain;
 using Caritas.Gestao.Repository;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Caritas.Gestao.ServiceAPI.Controllers
 {
@@ -22,31 +24,77 @@ namespace Caritas.Gestao.ServiceAPI.Controllers
         [HttpGet("filtro/{nome}")]
         public ActionResult GetUsers(string nome)
         {
-            
-            #region LINQ Method
-            var listUsers = _context.Users.Where(u => u.Name.Contains(nome)).ToList();
-            #endregion
+            try
+            {
+                #region LINQ Method
+                var listUsers = _context.Users.Where(u => u.Name.Contains(nome)).ToList();
+                #endregion
 
-            #region LINQ Query
-            //var listUsers = (from user in _context.Users
-            //                 where user.Name.Contains(nome)
-            //                 select user).ToList();
-            #endregion
+                #region LINQ Query
+                //var listUsers = (from user in _context.Users
+                //                 where user.Name.Contains(nome)
+                //                 select user).ToList();
+                #endregion
 
-            return Ok(listUsers);
+                return Ok(listUsers);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error: {ex}");
+            }
         }
 
-        [HttpPut("{id}")]
-        public void PutUsers(int id)
+        [HttpGet("GetUser")]
+        public ActionResult GetUser(string nome)
         {
-
+            try
+            {
+                return Ok(new UserModel());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error: {ex}");
+            }
         }
+
 
         [HttpPost("{id}")]
-        public void PostUsers(int id)
+        public ActionResult PostUsers([FromBody] UserModel user)
         {
+            try
+            {
+                _context.Users.Add(user);
+                _context.SaveChanges();
 
+                return Ok("OK");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error: {ex}");
+            }
         }
+
+
+        [HttpPut("PutUser/{id}")]
+        public ActionResult PutUser(int id, UserModel user)
+        {
+            try
+            {
+                if (_context.Users.AsNoTracking().FirstOrDefault(
+                    u => u.Id == id) != null)
+                {
+                    _context.Users.Update(user);
+                    _context.SaveChanges();
+                    return Ok(user);
+                }
+                return Ok("A Pesquisa não encontrou nenhum resultado");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error: {ex}");
+            }
+        }
+
 
         [HttpDelete("{id}")]
         public void DeleteUsers(int id)
--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Caritas.Gestao.Repository;
 using Microsoft.AspNetCore.Builder;
@@ -12,6 +14,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Swashbuckle.AspNetCore.SwaggerUI;
 
 namespace Caritas.Gestao.ServiceAPI
 {
@@ -32,6 +38,55 @@ namespace Caritas.Gestao.ServiceAPI
                 option.UseSqlServer(Configuration.GetConnectionString("CaritasDev"));
             });
             services.AddControllers();
+
+            //services.AddCustomizedSwagger();
+
+
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1.0", new OpenApiInfo
+            //    {
+            //        Title = "My APIs",
+            //        Version = "v1.0",
+            //        Description = "REST APIs "
+            //    });
+
+            //    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            //    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            //    c.IncludeXmlComments(xmlPath);
+            //});
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Caritás API", Version = "v1" });
+
+                var security = new Dictionary<string, IEnumerable<string>>
+                {
+                    {"Bearer", new string[] { }},
+                };
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authoriazation header using the bearer scheme",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                    Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] { }
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +106,13 @@ namespace Caritas.Gestao.ServiceAPI
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Caritas Rest API V1");
+                c.RoutePrefix = "swagger";
             });
         }
     }
